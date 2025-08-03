ab571bdf7889998490dafb1f2c1846c571085b3f
ahmad1064 <ahmadyz631@gmail.com>
2025-08-03 12:09:25 -0700

    GitHub File is pushed

---
+++ if.js
@@ -0,0 +1,31 @@
+// // Example 1
+// let userAge = +prompt("What is your age?");
+// if (userAge >= 18) {
+//     console.log("You can vote.")
+// } else {
+//     console.log("You cannot vote.");
+// }
+
+// // Example 2
+// let num = +prompt("Write any number");
+// if (num > 0) {
+//     console.log("The number is positive.");
+// } else {
+//     console.log("The number is negative.");
+// }
+
+// Example 3
+let userMarks = +prompt("Enter your marks");
+if (userMarks >= 90) {
+    console.log("Grade A");
+} else if (userMarks >= 80) {
+    console.log("Grade B");
+} else if (userMarks >= 70) {
+    console.log("Grade C");
+} else if (userMarks >= 60) {
+    console.log("Grade D");
+} else if (userMarks >= 50) {
+    console.log("Grade E");
+} else {
+    console.log("Fail");
+}
